cf0d48f4863d332d9a65151d7d2c326033a156af
Nathan <nlincoln@intellifarms.com>
2018-02-13 17:26:52 -0600

    Node 6 support

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import babel from "rollup-plugin-babel";
+
 export default {
   input: "./src/builder.js",
   output: {
     file: "./dist/bundle.js",
     format: "cjs"
-  }
+  },
+  plugins: [
+    babel({
+      exclude: "**/node_modules"
+    })
+  ]
 };
