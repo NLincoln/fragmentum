1f1728e11c92c4bb5783ce3ff9b356f841b9e23a
Nathan <nlincoln@intellifarms.com>
2018-03-01 21:58:42 -0600

    Support for pre-quoted strings

--- src/util/quote.js
+++ src/util/quote.js
@@ -1,12 +1,23 @@
+/**
+ *
+ * @param {String} val
+ * @param {*} opts
+ */
 export default function quote(val, opts = {}) {
   if (val === "*") {
     return val;
   }
   if (opts.single) {
+    if (val.startsWith(`'`)) {
+      return val;
+    }
     return `'${val.replace(`'`, `\\'`)}'`;
   }
   if (opts.parens) {
     return `(${val})`;
+  }
+  if (val.startsWith(`"`)) {
+    return val;
   }
   return `"${val.replace(`"`, `\\"`)}"`;
 }
--- src/util/quote.spec.js
+++ src/util/quote.spec.js
@@ -7,4 +7,14 @@ describe("quote", () => {
   test("ditto for single quotes", () => {
     expect(quote(`asdf'asdf`, { single: true })).toEqual(`'asdf\\'asdf'`);
   });
+  test.skip("composite column support", () => {
+    expect(quote("(table.composite).field")).toEqual(
+      `("table"."composite")."field"`
+    );
+    expect(quote("(table.composite).*")).toEqual(`("table"."composite").*`);
+  });
+  test("what if the quotes are already there???", () => {
+    expect(quote(`"asdf"`)).toBe(`"asdf"`);
+    expect(quote(`'asdf'`, { single: true })).toBe(`'asdf'`);
+  });
 });
